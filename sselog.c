e697ab2721c7bec8b2fc2f4ac4ffa008b70c3e92
Riccardo Gori <goriccardo@gmail.com>
2010-03-11 00:14:18 +0100

    Take care of denormalized numbers in log

--- sselog.c
+++ sselog.c
@@ -7,6 +7,7 @@
 
 PD_CONST(one, 1.);
 PD_CONST(half, 0.5);
+PI64_CONST(min_norm, 0x0010000000000000);
 
 /* CEPHES constants */
 PD_CONST(cephes_SQRTHF, 0.70710678118654752440);
@@ -34,6 +35,9 @@ __m128d e;
 /* Check for positiveness */
 __m128d invalid_mask = _mm_cmple_pd(x, _mm_setzero_pd());
 
+/* Avoid denormalized numbers */
+x = _mm_max_pd(x, *(__m128d*)pi64_min_norm);
+
 x = frexp_sse(x, &e);
 
 /*
@@ -43,56 +47,55 @@ x = frexp_sse(x, &e);
    } else { x = x - 1.0; }
 */
 __m128d mask = _mm_cmplt_pd(x, *(__m128d*)pd_cephes_SQRTHF);
-__m128d tmp = _mm_and_pd(x, mask);
+__m128d q = _mm_and_pd(x, mask);
 x = _mm_sub_pd(x, *(__m128d*)pd_one);
 e = _mm_sub_pd(e, _mm_and_pd(*(__m128d*)pd_one, mask));
-x = _mm_add_pd(x, tmp);
+x = _mm_add_pd(x, q);
 
 /* x^2 */
 __m128d z = _mm_mul_pd(x,x);
 
 /* Numeratore P(x) */
-__m128d y = _mm_mul_pd(x, *(__m128d*)pd_cephes_log_p0);
-y = _mm_add_pd(y, *(__m128d*)pd_cephes_log_p1);
-y = _mm_mul_pd(y, x);
-y = _mm_add_pd(y, *(__m128d*)pd_cephes_log_p2);
-y = _mm_mul_pd(y, x);
-y = _mm_add_pd(y, *(__m128d*)pd_cephes_log_p3);
-y = _mm_mul_pd(y, x);
-y = _mm_add_pd(y, *(__m128d*)pd_cephes_log_p4);
-y = _mm_mul_pd(y, x);
-y = _mm_add_pd(y, *(__m128d*)pd_cephes_log_p5);
+__m128d p = _mm_mul_pd(x, *(__m128d*)pd_cephes_log_p0);
+p = _mm_add_pd(p, *(__m128d*)pd_cephes_log_p1);
+p = _mm_mul_pd(p, x);
+p = _mm_add_pd(p, *(__m128d*)pd_cephes_log_p2);
+p = _mm_mul_pd(p, x);
+p = _mm_add_pd(p, *(__m128d*)pd_cephes_log_p3);
+p = _mm_mul_pd(p, x);
+p = _mm_add_pd(p, *(__m128d*)pd_cephes_log_p4);
+p = _mm_mul_pd(p, x);
+p = _mm_add_pd(p, *(__m128d*)pd_cephes_log_p5);
 
 /* Denominatore Q(x) */
-tmp = _mm_add_pd(x, *(__m128d*)pd_cephes_log_q1);
-tmp = _mm_mul_pd(tmp, x);
-tmp = _mm_add_pd(tmp, *(__m128d*)pd_cephes_log_q2);
-tmp = _mm_mul_pd(tmp, x);
-tmp = _mm_add_pd(tmp, *(__m128d*)pd_cephes_log_q3);
-tmp = _mm_mul_pd(tmp, x);
-tmp = _mm_add_pd(tmp, *(__m128d*)pd_cephes_log_q4);
-tmp = _mm_mul_pd(tmp, x);
-tmp = _mm_add_pd(tmp, *(__m128d*)pd_cephes_log_q5);
+q = _mm_add_pd(x, *(__m128d*)pd_cephes_log_q1);
+q = _mm_mul_pd(q, x);
+q = _mm_add_pd(q, *(__m128d*)pd_cephes_log_q2);
+q = _mm_mul_pd(q, x);
+q = _mm_add_pd(q, *(__m128d*)pd_cephes_log_q3);
+q = _mm_mul_pd(q, x);
+q = _mm_add_pd(q, *(__m128d*)pd_cephes_log_q4);
+q = _mm_mul_pd(q, x);
+q = _mm_add_pd(q, *(__m128d*)pd_cephes_log_q5);
 
 /* P(x)/Q(x) */
-y = _mm_div_pd(y,tmp);
+p = _mm_div_pd(p, q);
 
 /* x^3 * P(x)/Q(x) */
-y = _mm_mul_pd(y, z);
-y = _mm_mul_pd(y, x); 
+p = _mm_mul_pd(p, z);
+p = _mm_mul_pd(p, x);
 
 /* y = y - e*ec - z/2 */
-tmp = _mm_mul_pd(e, *(__m128d*)pd_cephes_log_ec1);
-__m128d tmp2 = _mm_mul_pd(z, *(__m128d*)pd_half);
-y = _mm_sub_pd(y, tmp);
-y = _mm_sub_pd(y, tmp2);
+q = _mm_mul_pd(e, *(__m128d*)pd_cephes_log_ec1);
+z = _mm_mul_pd(z, *(__m128d*)pd_half);
+p = _mm_sub_pd(p, q);
+p = _mm_sub_pd(p, z);
 
-tmp = _mm_mul_pd(e, *(__m128d*)pd_cephes_log_ec2);
-z = _mm_add_pd(x,y);
-z = _mm_add_pd(z,tmp);
+q = _mm_mul_pd(e, *(__m128d*)pd_cephes_log_ec2);
+z = _mm_add_pd(x,p);
+z = _mm_add_pd(z,q);
 
 z = _mm_or_pd(z, invalid_mask); // negative arg will be NAN
 
 return z;
 }
-
